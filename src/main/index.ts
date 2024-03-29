08eaa0dc11bb4a425e1a616986344a3b5b5df223
Vinicius Silva <viniciusgdr@outlook.com>
2024-03-28 23:19:58 -0300

    chore: remove code tests

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,22 +11,3 @@ export const makeFilmProvider = (type: Providers): FactoryContent => {
       throw new Error('Provider not found')
   }
 }
-
-void (async () => {
-  const vizer = makeFilmProvider('vizer')
-  // https://vizertv.in/serie/online/greys-anatomy
-  // https://vizertv.in/filme/online/five-nights-at-freddys-o-pesadelo-sem-fim
-  const result = await vizer.getInfo.get('https://vizertv.in/serie/online/greys-anatomy')
-  console.log(result)
-  if (result.movieType === 'serie') {
-    const episodes = await vizer.seasonEpisodes.load(result.seasons[0].dataSeasonId)
-    const player = await vizer.getPlayerEpisode.load(episodes[0].id)
-    console.log(player)
-    const download = await vizer.getDownloads.get(episodes[0].id, 'serie')
-    console.log(download)
-  } else {
-    console.log(result.players)
-    const download = await vizer.getDownloads.get(result.movieId, result.movieType)
-    console.log(download)
-  }
-})()
